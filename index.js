84763dd771b57c98d706ecb5edfb0dd586185727
MNM-Hussain <mnmhuss91@gmail.com>
2022-05-18 16:07:53 +0530

    "set API"

--- index.js
+++ index.js
@@ -6,9 +6,46 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+
+const bodyparser = require('body-parser');
+app.use(bodyparser.json());
+
+let { paymentData } = require("./paymentData");
+
 app.get('/', (req, res) => {
-  res.send('Hello Hussain, This is CTSE assignment 03');
+  res.send(paymentData);
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+
+
+
+app.get("/payment/:id", (req,res) => {
+  let id = +req.params.id;
+  let payment = paymentData.find((prod) => prod.id === id);
+  if(payment)
+      res.send(payment);
+  else
+      res.status(404).send("No Payment Found");
+})
+
+app.post("/payment/create", async (req,res) => {
+  let body = await req.body;
+  console.log(body);
+  let maxid = paymentData.reduce((acc , curr) => (curr.id >= acc ? curr.id : acc), 0);
+  let newid = maxid + 1;
+  let newPayment = { id: newid, ...body };
+  paymentData.push(newPayment);
+  res.send(newPayment);
+});
+
+app.delete("/payment/delete/:id", (req, res) => {
+  let id = +req.params.id;
+  let index = productData.findIndex((prod) => prod.id === id);
+  if(index >= 0) {
+      let deletedPayment = paymentData.splice(index, 1);
+      res.send(deletedPayment);
+  }else res.status(404).send("No Payment Found")
+})
+
---
+++ paymentData.js
@@ -0,0 +1,7 @@
+let payment = [
+    { id: 1, price: 50, CardHolderName: "Hussain", cardNumber: 1002220001, cvc:456, expireDate:'03/2023'},
+    { id: 2, price: 150, CardHolderName: "John", cardNumber: 2002220001, cvc:056, expireDate:'06/2023'},
+    { id: 3, price: 250, CardHolderName: "keelBurg", cardNumber: 3002220001, cvc:956, expireDate:'09/2023'},
+  ]
+  
+  module.exports.paymentData = payment;
